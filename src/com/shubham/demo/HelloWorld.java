510921d12fb3ebcec1fe088775fae29cac1fab16
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-06 09:47:30 +0530

    adding hello world program

---
+++ src/com/shubham/demo/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.shubham.demo;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, This is 1st BridgeLabz Java Program");
+    }
+}
